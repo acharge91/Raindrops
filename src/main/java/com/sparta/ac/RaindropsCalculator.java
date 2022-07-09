cd653df2814d26ad986a3828e79317348c34cbe1
acharge91 <ashtoncharge@gmail.com>
2022-07-09 09:49:57 +0100

    implemented the factor calculator functionality and the string builder method

---
+++ src/main/java/com/sparta/ac/RaindropsCalculator.java
@@ -0,0 +1,29 @@
+package com.sparta.ac;
+
+import java.util.ArrayList;
+
+public class RaindropsCalculator {
+
+    public static String raindropsStringBuilder(int numToFactor) {
+        ArrayList<Integer> factorArray = new ArrayList<>();
+        factorCalculator(numToFactor, numToFactor, factorArray);
+
+        System.out.println(factorArray);
+
+        String returnString = "";
+        if (factorArray.contains(3)) returnString += "Pling";
+        if (factorArray.contains(5)) returnString += "Plang";
+        if (factorArray.contains(7)) returnString += "Plong";
+        if (returnString.isEmpty()) returnString += numToFactor;
+
+        return returnString;
+    }
+
+    public static void factorCalculator(int numToFactor, int numIncrements, ArrayList<Integer> factorArray) {
+        if (numIncrements == 0) return;
+        if (numToFactor % numIncrements == 0 ) factorArray.add(numIncrements);
+
+        numIncrements--;
+        factorCalculator(numToFactor, numIncrements, factorArray);
+    }
+}
